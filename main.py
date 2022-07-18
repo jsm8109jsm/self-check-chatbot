6ab03183ad04b97205538e407e8e07a372af04e2
jsm8109jsm <jsm8109jsm@gmail.com>
2022-07-18 15:18:46 +0900

    url 들어가기

---
+++ main.py
@@ -0,0 +1,9 @@
+import pyautogui
+import webbrowser
+
+webbrowser.open('https://hcs.eduro.go.kr/#/loginHome')
+
+# i = pyautogui.locateCenterOnScreen('file.png')
+# pyautogui.click(i)
+
+print(pyautogui.position())
