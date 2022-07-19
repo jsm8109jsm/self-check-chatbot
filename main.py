0e000ef1987c20bff6f1aef12b0c86431ca5dca7
jsm8109jsm <jsm8109jsm@gmail.com>
2022-07-19 15:08:17 +0900

    city 딕셔너리 추가, 이름, 생일 입력

--- main.py
+++ main.py
@@ -1,9 +1,53 @@
 import pyautogui
+import time
 import webbrowser
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-webbrowser.open('https://hcs.eduro.go.kr/#/loginHome')
+username = input('이름을 입력하세요(예시 : 정승민) : ')
+birthday = input('생일을 입력하세요(예시 : 050203) : ')
 
-# i = pyautogui.locateCenterOnScreen('file.png')
-# pyautogui.click(i)
+city = {
+    "서울특별시": 1,
+    "부산광역시": 2,
+    "대구광역시": 3,
+    "인천광역시": 4,
+    "광주광역시": 5,
+    "대전광역시": 6,
+    "울산광역시": 7,
+    "세종특별자치시": 8,
+    "경기도": 9,
+    "강원도": 10,
+    "충청북도": 11,
+    "충청남도": 12,
+    "전라북도": 13,
+    "전라남도": 14,
+    "경상북도": 15,
+    "경상남도": 16,
+    "제주특별자치도": 17
+}
 
-print(pyautogui.position())
+driver = webdriver.Chrome()
+url = 'https://hcs.eduro.go.kr/#/loginHome'
+driver.get(url)
+
+time.sleep(1)
+
+# go = pyautogui.locateCenterOnScreen('images/go.png', confidence=0.8)
+# pyautogui.click(go)
+
+driver.find_element(By.ID, 'btnConfirm2').click()
+
+# driver.find_element(By.ID, 'user_name_input').click()
+driver.find_element(By.ID, 'schul_name_input').click()
+pyautogui.typewrite(['enter'])
+driver.find_element(By.ID, 'user_name_input').send_keys(username)
+driver.find_element(By.ID, 'birthday_input').send_keys(birthday)
+
+# time.sleep(0.5)
+# name = pyautogui.locateCenterOnScreen('images/name.png', confidence=0.8)
+# pyautogui.click(name)
+# time.sleep(1)
+# print(a)
+# pyautogui.typewrite(a)
